f2643c46d776b05b95f54504ac8408bf36909a9d
Alexander Rovang <wolf_vii@yahoo.com>
2021-08-14 16:51:44 -0400

    adds actions in controller

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,37 @@
 class MembersController < ApplicationController
+
+  def index
+    member = Member.all 
+    render json: member
+  end
+
+  def show
+    member = Member.find_by(id: params[:id])
+    render json: member
+  end
+ 
+  def update
+    member = Member.find_by(id: params[:id])
+    member.update(member_params)
+    if member.save
+      render json: member
+    else
+      render json: {error: "uh oh"}
+    end
+  end
+
+  private
+
+  def member_params
+    params.require("member").permit(
+      :name,
+      :title,
+      :bio,
+      :image_url,
+      :votes,
+      :id,
+      :created_at, 
+      :updated_at
+    )
+  end
 end
