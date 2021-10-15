c767cbba9bb9087b15bfda5da711c56b2a9855d3
Alexander Rovang <wolf_vii@yahoo.com>
2021-10-15 15:16:38 -0500

    adds small readme

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,14 @@
 require 'rest-client'
 
 members = RestClient.get 'https://coding-assignment.g2crowd.com/'
-# binding.pry 
+
 members_array = JSON.parse(members)
-# binding.pry 
+
 members_array.each do |member|
   Member.create(
     name: member["name"],
     title: member["title"],
     bio: member["bio"],
-    # votes: member["votes"],
     image_url: member["image_url"],
 
   )
